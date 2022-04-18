cefd0608e45f4aa512d8039e57db8ed3ee0dc253
Vitor Fontenele <vitor_fontenele@hotmail.com>
2022-04-17 22:48:26 -0300

    Update calculator

--- front-end-dev-libraries/front-end-dev-lib-projects/calculator/module.js
+++ front-end-dev-libraries/front-end-dev-lib-projects/calculator/module.js
@@ -1,17 +1,34 @@
 $(document).ready(function () {
-    var currentValue = 10;
+    //var currentValue = 10;
     var currentOperation = "";
     var operators = ["+","-","/","*"];
 
     $("#clear").click(function(){
         $("#result").text(0);
         currentOperation = "";
+        $("#operation").text(currentOperation);
     })
 
     $(".operator").click(function(){
-        let lastElOfCurrOp = currentOperation[currentOperation.length - 1];
-        let disallowOp = operators.filter(item => item == lastElOfCurrOp).length;
-        if (!Boolean(disallowOp)){
+        if (currentOperation == ""){
+            currentOperation = 0;
+        }
+        let operator = $(this).attr("value");
+        let lastEl = currentOperation[currentOperation.length - 1];
+        let secondToLastEl = currentOperation[currentOperation.length - 2];
+        let lastIsOperator = operators.includes(lastEl);
+        let secondToLastIsOperator = operators.includes(secondToLastEl);
+        if (lastIsOperator){
+            if (secondToLastIsOperator){
+                if (operator != '-'){
+                    currentOperation = currentOperation.slice(0,- 2) + $(this).attr("value"); 
+                }
+            } else if (operator == '-') {
+                currentOperation += $(this).attr("value");
+            } else {
+                currentOperation = currentOperation.slice(0,- 1) + $(this).attr("value");
+            }
+        } else{
             currentOperation += $(this).attr("value");
         }
         $("#operation").text(currentOperation);
@@ -19,6 +36,23 @@ $(document).ready(function () {
 
     $(".number").click(function(){
         currentOperation += $(this).attr("value");
+        $("#operation").text(currentOperation);
+    })
+
+    $("#equals").click(function(){
+        let lastEl = currentOperation[currentOperation.length - 1];
+        let secondToLastEl = currentOperation[currentOperation.length - 2];
+        let lastIsOperator = operators.includes(lastEl);
+        let secondToLastIsOperator = operators.includes(secondToLastEl);
+        if (lastIsOperator && secondToLastIsOperator){
+            currentOperation = currentOperation.slice(0,- 2) + $(this).attr("value");
+        } else if (lastIsOperator){
+            currentOperation = currentOperation.slice(0,- 1) + $(this).attr("value");
+        }
+        /*
+        if (replaceLast){
+            currentOperation = currentOperation.slice(0, currentOperation.length - 1) + $(this).attr("value");
+        } */
         $("#operation").text(currentOperation);
     })
 });
